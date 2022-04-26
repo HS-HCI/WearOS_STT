64118ec93e4e013d22e8c6c7b5a6e8a190b40aaf
nahyunii <rlaskgus03@hs.ac.kr>
2022-04-26 18:17:25 +0900

    FirstCommit

---
+++ VoiceRecognitionSample/app/src/main/java/com/example/glass/voicerecognitionsample/OkGoogleActivity.java
@@ -0,0 +1,28 @@
+package com.example.glass.voicerecognitionsample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ListView;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.glass.ui.GlassGestureDetector;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class OkGoogleActivity extends AppCompatActivity {
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.okgoogle);
+        text =(TextView) findViewById(R.id.textView1);
+        text.setText("This app is developed by  Hyoseock " +
+                " Nahyun  " +
+                "Siyeon   ");
+    }
+}
