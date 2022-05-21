9b7c95ead2167b0e57de8afbf74451a08086652e
nahyunii <rlaskgus03@hs.ac.kr>
2022-05-21 12:34:24 +0900

    Video_Test

--- VoiceRecognitionSample/app/src/main/java/com/example/glass/voicerecognitionsample/MediaActivity.java
+++ VoiceRecognitionSample/app/src/main/java/com/example/glass/voicerecognitionsample/MediaActivity.java
@@ -7,16 +7,62 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.VideoView;
 import com.example.glass.ui.GlassGestureDetector;
 
+import java.io.IOException;
+
 public class MediaActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private SurfaceHolder holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media);
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.cat);
-        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        mediaPlayer = new MediaPlayer();
+
+        try{
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/raw/cat");
+            mediaPlayer.setDataSource(this, uri);
+            holder=surfaceView.getHolder();
+            holder.addCallback(new MyCallBack());
+            mediaPlayer.prepare();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    mediaPlayer.start();
+                    mediaPlayer.setLooping(true);
+                }
+            });
+        }
+        catch (IOException e) {}
+
+    }
+    private class MyCallBack implements SurfaceHolder.Callback {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            mediaPlayer.setDisplay(holder);
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
     }
 }
+
+
